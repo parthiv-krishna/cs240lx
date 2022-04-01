d05dd2bba717048e14d4e8c21432ece0c226222c
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2022-03-31 18:55:48 -0700

    implement read_file

--- libunix/read-file.c
+++ libunix/read-file.c
@@ -25,5 +25,30 @@
 // make sure to close the file descriptor (this will
 // matter for later labs).
 void *read_file(unsigned *size, const char *name) {
-    unimplemented();
+    struct stat statbuf;
+    if (stat(name, &statbuf) < 0) {
+        sys_die(stat, "read_file failed to stat file");
+    }
+
+    *size = statbuf.st_size;
+    // round up to multiple of 4
+    off_t roundup = (*size + 3) & ~0b111;
+
+    void *data = calloc(roundup, 1);
+    if (!data) {
+        sys_die(calloc, "read_file failed to calloc buffer");
+    }
+
+    int fd = open(name, O_RDONLY);
+    if (fd < 0) {
+        sys_die(open, "read_file failed open file");
+    }
+    if (read(fd, data, *size) < 0) {
+        sys_die(read, "read_file failed to read file");
+    }
+    
+    if (close(fd) < 0) {
+        sys_die(close, "read_file failed to close file");
+    }
+    return data;
 }
