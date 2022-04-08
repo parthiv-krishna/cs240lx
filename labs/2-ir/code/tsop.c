df66085f09db1cda308d9bf8362832264274351f
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2022-04-07 19:58:32 -0700

    working polling-based IR decode

--- labs/2-ir/code/tsop.c
+++ labs/2-ir/code/tsop.c
@@ -6,18 +6,57 @@
 #include "rpi.h"
 enum { ir_eps = 200, 
        timeout_us = 20000,
-       delay_0 = 4500, 
-       delay_1 = 4500 
+       header_delay_0 = 4500, 
+       header_delay_1 = 4500,
+       skip_delay = 550,
+       zero_delay = 450,
+       one_delay = 1450
     };
 
 // we should never get this.
-enum { NOISE = 0 } ;
+enum { INVALID_HEADER = 0xdeadbeef,
+       POWER = 0x3c201de2,
+       SOURCE = 0x3c215ca2,
+       VOL_UP = 0x3c21dc22,
+       VOL_DOWN = 0x3c203dc2,
+       MUTE = 0x3c211ce2,
+       REPLAY = 0x3c2122dc,
+       SEEK_BACK = 0x3c21a25c,
+       PLAY_PAUSE = 0x3c20a35c,
+       SEEK_FORWARD = 0x3c20639c,
+       SOUND_EFFECT = 0x3c21bc42,
+       SOUND = 0x3c2102fc,
+       BT_POWER = 0x3c21728c,
+       ARROW_LEFT = 0x3c201be4,
+       SOUND_CTRL = 0x3c20cb34,
+       ARROW_RIGHT = 0x3c211ae4
+     };
+
+#define KEY_TO_STR_CASE(value) \
+            case value: return #value;
 
 struct readings { unsigned usec, v; };
 
 const char *key_to_str(unsigned x) {
-    output("convert an input value to a string\n");
-    unimplemented();
+    switch (x) {
+        KEY_TO_STR_CASE(INVALID_HEADER)
+        KEY_TO_STR_CASE(POWER)
+        KEY_TO_STR_CASE(SOURCE)
+        KEY_TO_STR_CASE(VOL_UP)
+        KEY_TO_STR_CASE(VOL_DOWN)
+        KEY_TO_STR_CASE(MUTE)
+        KEY_TO_STR_CASE(REPLAY)
+        KEY_TO_STR_CASE(SEEK_BACK)
+        KEY_TO_STR_CASE(PLAY_PAUSE)
+        KEY_TO_STR_CASE(SEEK_FORWARD)
+        KEY_TO_STR_CASE(SOUND_EFFECT)
+        KEY_TO_STR_CASE(SOUND)
+        KEY_TO_STR_CASE(BT_POWER)
+        KEY_TO_STR_CASE(ARROW_LEFT)
+        KEY_TO_STR_CASE(SOUND_CTRL)
+        KEY_TO_STR_CASE(ARROW_RIGHT)
+        default: return "NOISE";
+    }
 }
 
 // adapt your read_while_equal: return 0 if timeout passed, otherwise
@@ -40,23 +79,25 @@ static int abs(int x) {
 
 // return 0 if e is closer to <lb>, 1 if its closer to <ub>
 static int pick(struct readings *e, unsigned lb, unsigned ub) {
-    unimplemented();
+    return abs(e->usec - lb) < abs(e->usec - ub) ? 0 : 1;
     panic("invalid time: <%d> expected %d or %d\n", e->usec, lb, ub);
+}
+
+// return 1 if a and b are within ir_eps
+static int close(unsigned a, unsigned b) {
+    return (abs(a - b) <= ir_eps);
 }
 
 // return 1 if is a skip: skip = delay of 550-/+eps
 static int is_skip(struct readings *e) {
-    unimplemented();
-    return 0;
+    return close(e->usec, skip_delay);
 }
 
 // header is a delay of 9000 and then a delay of 4500
 int is_header(struct readings *r, unsigned n) {
     if(n < 2)
         return 0;
-    int zero = abs(r[0].usec - delay_0) <= ir_eps;
-    int one = abs(r[1].usec - delay_1) <= ir_eps;
-    return (zero && one);
+    return (close(r[0].usec, header_delay_0) && close(r[1].usec, header_delay_1));
 }
 
 // convert <r> into an integer by or'ing in 0 or 1 depending on the 
@@ -64,7 +105,20 @@ int is_header(struct readings *r, unsigned n) {
 //
 // assert that they are seperated by skips!
 unsigned convert(struct readings *r, unsigned n) {
-    unimplemented();
+    unsigned result = 0;
+    if (!is_header(r, n)) {
+        return INVALID_HEADER;
+    }
+    for (int i = 2; i < n; i++) {
+        if (i % 2 == 0) {
+            assert(is_skip(r + i));
+        } else {
+            if (pick(r + i, zero_delay, one_delay)) {
+                result |= 1 << ((n - i) / 2);
+            }
+        }
+    }
+    return result;
 }
 
 static void print_readings(struct readings *r, int n) {
@@ -123,7 +177,6 @@ void notmain(void) {
         int n = get_readings(in, r, N);
 
         output("done getting readings\n");
-        print_readings(r, n);
     
         unsigned x = convert(r,n);
         output("converted to %x\n", x);
