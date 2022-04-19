a17585a170f3c0c0139d6ad6dc7a9ef7033c7bf6
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2022-04-19 02:33:18 -0700

    add asserts to i2s init. works?

--- labs/4-ws2812b/code/i2s.c
+++ labs/4-ws2812b/code/i2s.c
@@ -1,17 +1,5 @@
 #include "i2s.h"
 
-
-// Set the EN bit to enable the PCM block. Set all operational values to define the frame and
-// channel settings. Assert RXCLR and/or TXCLR wait for 2 PCM clocks to ensure the
-// FIFOs are reset. The SYNC bit can be used to determine when 2 clocks have passed.
-// Set RXTHR/TXTHR to determine the FIFO thresholds.
-
-// If transmitting, ensure that sufficient sample words have been written to PCMFIFO
-// before transmission is started. Set TXON and/or RXON to begin operation. Poll
-// TXW writing sample words to PCMFIFO and RXR reading sample words from
-// PCMFIFO until all data is transferred.
-
-
 i2s_regs_t *i2s_regs = (i2s_regs_t *)I2S_REGS_BASE;
 cm_regs_t *cm_regs = (cm_regs_t *)CM_REGS_BASE;
 
@@ -28,14 +16,17 @@ void i2s_init(void) {
     dev_barrier(); 
 
     PUT32(addr(cm_regs->pcm_ctrl), CM_REGS_MSB | CM_CTRL_XTAL);
+    assert(GET32(addr(cm_regs->pcm_ctrl)) == CM_CTRL_XTAL);
 
     // See https://elinux.org/BCM2835_datasheet_errata#p107-108_table_6-35
     // and BCM2835 peripheral manual page 105 (section 6.3)
     // I think we can get away with an integer divisor of 4
     PUT32(addr(cm_regs->pcm_div), CM_REGS_MSB | (4 << 12));
+    assert(GET32(addr(cm_regs->pcm_div)) == (4 << 12));
 
     // enable the clock manager
     PUT32(addr(cm_regs->pcm_ctrl), CM_REGS_MSB | CM_CTRL_EN | CM_CTRL_XTAL);
+    assert(GET32(addr(cm_regs->pcm_ctrl)) == (CM_CTRL_EN | CM_CTRL_XTAL));
 
     // ensure writes to clock manager are complete since we are going to i2s
     dev_barrier();
@@ -47,6 +38,7 @@ void i2s_init(void) {
     mode = bits_set(mode, I2S_MODE_FSLEN_LB, I2S_MODE_FSLEN_UB, 32);
     mode = bits_set(mode, I2S_MODE_FLEN_LB, I2S_MODE_FLEN_UB, 63);
     PUT32(addr(i2s_regs->mode), mode);
+    assert(GET32(addr(i2s_regs->mode)) == mode);
 
     // set channel width: BCM peripherals page 132
     // total width:  (CH1WEX* 16) + CH1WID + 8
@@ -56,22 +48,25 @@ void i2s_init(void) {
     rxc = bits_set(rxc, I2S_RXC_CH1WID_LB, I2S_RXC_CH1WID_UB, 8);
     rxc = bit_set(rxc, I2S_RXC_CH1WEX);
     PUT32(addr(i2s_regs->rx_cfg), rxc);
+    assert(GET32(addr(i2s_regs->rx_cfg)) == rxc);
 
     uint32_t cs = 0;
     cs = bit_set(cs, I2S_CS_STBY); // disable standby
     cs = bit_set(cs, I2S_CS_RXCLR); // clear receive FIFO
     cs = bit_set(cs, I2S_CS_RXON); // enable receive
     PUT32(addr(i2s_regs->cs), cs);
+    cs = GET32(addr(i2s_regs->cs));
+    assert(bit_is_on(cs, I2S_CS_STBY));
+    assert(bit_is_on(cs, I2S_CS_RXON));
 
     cs = GET32(addr(i2s_regs->cs));
-    bit_set(cs, I2S_CS_EN); // enable i2s
+    cs = bit_set(cs, I2S_CS_EN); // enable i2s
     PUT32(addr(i2s_regs->cs), cs);
 
     dev_barrier();
 
     cs = GET32(addr(i2s_regs->cs));
-    demand(bit_is_on(cs, I2S_CS_EN), "i2s not enabled %x", cs);
+    assert(bit_is_on(cs, I2S_CS_EN));
 
     dev_barrier();
-
 }
