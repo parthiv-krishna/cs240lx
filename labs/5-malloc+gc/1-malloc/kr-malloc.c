e92ff64914b6c6896d4adeebf565971257ff620f
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2022-04-19 20:00:32 -0700

    make kr less awful

--- labs/5-malloc+gc/1-malloc/kr-malloc.c
+++ labs/5-malloc+gc/1-malloc/kr-malloc.c
@@ -28,44 +28,47 @@ static Header *freep = NULL;
 /* minimum #units to request */
 
 /* morecore: ask system for more memory */
-static Header *morecore(unsigned nu) {
-    char *cp;
-    Header *up;
-    if (nu < NALLOC)
-        nu = NALLOC;
-    cp = sbrk(nu * sizeof(Header));
-    if (cp == (char *)-1) {
+static Header *morecore(unsigned numunits) {
+    if (numunits < NALLOC) {
+        numunits = NALLOC;
+    }
+
+    void *void_ptr = sbrk(numunits * sizeof(Header));
+    if (void_ptr == (void *)-1) {
+        // this shouldn't happen, it'll just panic
         /* no space at all */
         return NULL;
     }
-    up = (Header *)cp;
-    up->s.size = nu;
-    kr_free((void *)(up + 1));
+
+    Header *header_ptr = (Header *)void_ptr;
+    header_ptr->s.size = numunits;
+    kr_free((void *)(header_ptr + 1));
     return freep;
 }
 
 /* malloc: general-purpose storage allocator */
-void *kr_malloc(unsigned nbytes)
-{
+void *kr_malloc(unsigned nbytes) {
+    unsigned nunits = (nbytes + sizeof(Header) - 1) / sizeof(Header) + 1;
 
-    Header *p, *prevp;
-    Header *moreroce(unsigned);
-    unsigned nunits;
-    nunits = (nbytes + sizeof(Header) - 1) / sizeof(Header) + 1;
-    if ((prevp = freep) == NULL)
+    Header *prevp = freep;
+    if (freep == NULL)
     {
         /* no free list yet */
-        base.s.ptr = freep = prevp = &base;
+        freep = &base;
+        prevp = freep;
+
+        base.s.ptr = freep;
         base.s.size = 0;
     }
-    for (p = prevp->s.ptr;; prevp = p, p = p->s.ptr)
-    {
-        if (p->s.size >= nunits)
-        {                            /* big enough */
-            if (p->s.size == nunits) /* exactly */
+
+    Header *p = prevp->s.ptr;
+    while (1) {
+        if (p->s.size >= nunits){                            
+            /* big enough */
+            if (p->s.size == nunits) {
+                /* exactly */
                 prevp->s.ptr = p->s.ptr;
-            else
-            {
+            } else { 
                 /* allocate tail end */
                 p->s.size -= nunits;
                 p += p->s.size;
@@ -74,37 +77,45 @@ void *kr_malloc(unsigned nbytes)
             freep = prevp;
             return (void *)(p + 1);
         }
-        if (p == freep) /* wrapped around free list */
-            if ((p = morecore(nunits)) == NULL)
+        if (p == freep) {
+            /* wrapped around free list */
+            if ((p = morecore(nunits)) == NULL) {
                 return NULL;
-        /* none left */
+                /* none left */
+            }
+        }
+        prevp = p;
+        p = p->s.ptr;
     }
 }
 
 /* free: put block ap in free list */
-void kr_free(void *ap)
-{
-    Header *bp, *p;
-    bp = (Header *)ap - 1;
+void kr_free(void *ap) {
+    
+    Header *bp = (Header *)ap - 1;
     /* point to block header */
+    
+    Header *p;
     for (p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
         if (p >= p->s.ptr && (bp > p || bp < p->s.ptr))
             break; /* freed block at start or end of arena */
-    if (bp + bp->s.size == p->s.ptr)
-    {
+    if (bp + bp->s.size == p->s.ptr) {
         /* join to upper nbr */
         bp->s.size += p->s.ptr->s.size;
         bp->s.ptr = p->s.ptr->s.ptr;
     }
-    else
+    else {
         bp->s.ptr = p->s.ptr;
-    if (p + p->s.size == bp)
-    {
+    }
+    
+    if (p + p->s.size == bp) {
         /* join to lower nbr */
         p->s.size += bp->s.size;
         p->s.ptr = bp->s.ptr;
     }
-    else
+    else {
         p->s.ptr = bp;
+    }
+    
     freep = p;
 }
