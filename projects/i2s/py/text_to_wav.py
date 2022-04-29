1ad976f309200f9c1547c6ea3d48546540a3ca2f
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2022-04-28 20:01:19 -0700

    hello world wav

--- projects/i2s/py/text_to_wav.py
+++ projects/i2s/py/text_to_wav.py
@@ -2,10 +2,15 @@ import scipy.io.wavfile as wav
 import numpy as np
 
 
-data = np.genfromtxt("dump.txt")
+data = np.genfromtxt("dump.txt", converters={0: lambda s: int(s, 16)})
 print(data.shape)
 
-sample_rate = int(np.round(44100/(588008e-6)))
+data = data - 2**31
+data = data.astype(np.int32)
+
+# sample_rate = int(np.round(44100/(1000016e-6)))
+print(np.min(data), np.max(data))
+sample_rate = 44100
 print(sample_rate)
 
 wav.write("dump.wav", sample_rate, data)
