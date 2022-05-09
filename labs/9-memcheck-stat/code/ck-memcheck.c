477093ed87a437aa70d89d58689a82af7b434212
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2022-05-09 13:54:24 -0700

    ck-memcheck runs timer, baseline single_transfer

--- labs/9-memcheck-stat/code/ck-memcheck.c
+++ labs/9-memcheck-stat/code/ck-memcheck.c
@@ -3,8 +3,11 @@
 #include "rpi-internal.h"
 #include "timer-interrupt.h"
 #include "ckalloc.h"
+#include "vector-base.h"
+#include "breakpoint.h"
 
-#define TIMER_INT_NCYCLES 10000
+
+#define TIMER_INT_NCYCLES 0
 
 // you'll need to pull your code from lab 2 here so you
 // can fabricate jumps
@@ -49,21 +52,96 @@ unsigned ck_mem_stats(int clear_stats_p) {
     return c;
 }
 
+typedef enum {
+    MEM_OP_NONE = 0,
+    MEM_OP_SINGLE = 1,
+    MEM_OP_MULTIPLE = 2
+} mem_op_t;
+
+mem_op_t memory_op_type(uint32_t instr) {
+    if (((instr >> 26) & 0b11) == 0b01) {
+        // single transfer, bits 27:26 = 0b01
+        return MEM_OP_SINGLE;
+    }
+
+    if (((instr >> 25) & 0b111) == 0b100) {
+        // multiple transfer, bits 27:25 = 100
+        return MEM_OP_MULTIPLE;
+    }
+    return MEM_OP_NONE;
+}
+
+void validate_single_transfer(uint32_t instr, uint32_t *pc, uint32_t *regs) {
+    unsigned Rn = (instr >> 16) & 0b1111;
+    if (Rn == 15) {
+        // pc-relative addressing.  we don't check this.
+        return;
+    }
+
+    uint32_t *base = (uint32_t *) regs[Rn];
+
+    uint32_t offset = 0;
+    uint32_t *access = base + offset;
+
+    if (!ck_ptr_is_alloced(base)) {
+        panic("invalid access to unallocated memory: pc=%x, base=%x, offset=%d\n", pc, base, offset);
+    }
+}
+
 // note: lr = the pc that we were interrupted at.
 // longer term: pass in the entire register bank so we can figure
 // out more general questions.
-void ck_mem_interrupt(uint32_t pc) {
+void ck_mem_interrupt(uint32_t* pc, uint32_t *regs) {
+    // we don't know what the user was doing.
 
-    // we don't know what the user was doing.
+    uint32_t instr = *pc;
+    mem_op_t type = memory_op_type(instr);
+    if (type == MEM_OP_NONE) {
+        return; // not a memory op
+    }
+    if (type == MEM_OP_SINGLE) {
+        // single transfer
+        validate_single_transfer(instr, pc, regs);
+    }
+    if (type == MEM_OP_MULTIPLE) {
+        
+    }
+
+}
+
+void interrupt_vector(unsigned pc) {
+    uint32_t *regs;
+    asm volatile("mov %0, r1" : "=r"(regs));
     dev_barrier();
+    // printk("abort\n");
+    // system_disable_interrupts();
+
+    // confirm we are in timer interrupt
+    unsigned pending = GET32(IRQ_basic_pending);
+    if((pending & RPI_BASIC_ARM_TIMER_IRQ) == 0) {
+        dev_barrier();
+        // system_enable_interrupts();
+        return;
+    }
+
+    // printk("%p %x %d\n", pc, *(uint32_t *)pc, memory_op_type(*(uint32_t *)pc));
+
     if (check_on) {
-        if (ck_mem_checked_pc(pc)) {
-            checked++;
-        } else {
+        if (!ck_mem_checked_pc(pc)) {
             skipped++;
+        } else {
+            ck_mem_interrupt((uint32_t*)pc, regs);
+            checked++;
         }
-    }  
+    }
+    // printk("\n");
+
+    // clear the interrupt
+    PUT32(arm_timer_IRQClear, 1);
+    // system_enable_interrupts();
     // we don't know what the user was doing.
+
+    // brkpt_mismatch_set(pc);
     dev_barrier();
 }
 
@@ -77,9 +155,11 @@ void ck_mem_init(void) {
     assert(in_range((uint32_t)ckfree, start_nocheck, end_nocheck));
     assert(!in_range((uint32_t)printk, start_nocheck, end_nocheck));
 
-    timer_interrupt_init(TIMER_INT_NCYCLES);
     skipped = 0;
     checked = 0;
+
+    extern uint32_t _interrupt_table;
+    vector_base_set(&_interrupt_table);
 }
 
 // only check pc addresses [start,end)
@@ -94,13 +174,22 @@ void ck_mem_on(void) {
     assert(init_p && !check_on);
     check_on = 1;
 
-    // unimplemented();
+    timer_interrupt_init(TIMER_INT_NCYCLES);
+    system_enable_interrupts();
+
+    // uint32_t pc;
+    // asm volatile("mov %0, r15" : "=r" (pc));
+    // brkpt_mismatch_set(pc);
+    // brkpt_mismatch_start();
+    // asm volatile("cps 16");
+    // system_enable_interrupts();
 }
 
 // maybe should always do the heap check at the end.
 void ck_mem_off(void) {
     assert(init_p && check_on);
-
-    // unimplemented();
     check_on = 0;
+    system_disable_interrupts();
+    // brkpt_mismatch_stop();
+
 }
