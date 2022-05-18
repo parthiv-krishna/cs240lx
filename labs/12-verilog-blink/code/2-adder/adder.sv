11dc694666c6c81f0ab5e7a8325c6cbb2f426355
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2022-05-17 18:13:21 -0700

    adder

--- labs/12-verilog-blink/code/2-adder/adder.sv
+++ labs/12-verilog-blink/code/2-adder/adder.sv
@@ -3,7 +3,30 @@ module adder
         input logic [31:0] a, b,
         output logic [31:0] sum
     );
-    // TODO
+
+    // turn off linter
+    /* verilator lint_off UNOPTFLAT */ 
+    wire [31:0] carry;
+
+    half_adder lsb(
+        .a(a[0]),
+        .b(b[0]),
+        .s(sum[0]),
+        .c(carry[0])
+    );
+
+    generate
+        genvar i;
+        for (i = 1; i < 32; i = i + 1) begin
+            full_adder bit_u(
+                .a(a[i]),
+                .b(b[i]),
+                .cin(carry[i-1]),
+                .s(sum[i]),
+                .cout(carry[i])
+            );
+        end
+    endgenerate
 endmodule
 
 module half_adder
@@ -11,7 +34,8 @@ module half_adder
         input logic a, b,
         output logic c, s
     );
-    // TODO
+    assign c = a & b;
+    assign s = a ^ b;
 endmodule
 
 module full_adder
@@ -19,5 +43,6 @@ module full_adder
         input logic a, b, cin,
         output logic cout, s
     );
-    // TODO
+    assign cout = (a & b) | (a & cin) | (b & cin);
+    assign s = a ^ b ^ cin;
 endmodule
