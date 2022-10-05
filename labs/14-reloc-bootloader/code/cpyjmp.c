dc8097398adad5aa9eef99e091313047acdc00d4
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2022-10-05 16:35:07 -0700

    cpyjmp

--- labs/14-reloc-bootloader/code/cpyjmp.c
+++ labs/14-reloc-bootloader/code/cpyjmp.c
@@ -15,10 +15,17 @@
 //    won't work
 //  - most likely should (but we don't) switch stacks to be safe during 
 //    the copy.  this is probably the biggest possible source of bugs atm.
-static inline void 
+__attribute__((always_inline)) static inline void 
 cpyjmp_internal(uint32_t code_dst, const void *code, unsigned nbytes) {
     // copy [code, code+nbytes) to <code_dst> and jump to <code_dst>.
-    unimplemented();
+    char *code_char = (char *)code;
+    char *code_dst_char = (char *)code_dst;
+    for (unsigned i = 0; i < nbytes; i++) {
+        code_dst_char[i] = code_char[i];
+    }
+
+    cpyjmp_t func = (cpyjmp_t)code_dst;
+    func(code_dst, code, nbytes);
 
     // the jump to <code_dst> should never return.
     // need a better safety net.
@@ -34,19 +41,39 @@ void cpyjmp(uint32_t code_dst, const void *code, unsigned nbytes) {
     cpyjmp_internal(code_dst,code,nbytes);
 }
 
+void cpyjmp_end(void) {
+}
+
+void cpyjmp2(uint32_t code_dst, const void *code, unsigned nbytes) {
+    cpyjmp_internal(code_dst,code,nbytes);
+}
+
+void cpyjmp2_end(void) {
+}
 
 
 // do some simple sanity checks that the generated binary looks as we
 // expect.
 void cpyjmp_check(void) {
     // check 1: make sure the two routines are the same size.
+    int cpyjmp_size = (int *)cpyjmp_end - (int *)cpyjmp;
+    int cpyjmp2_size = (int *)cpyjmp2_end - (int *)cpyjmp2;
+    assert(cpyjmp_size == cpyjmp2_size);
+
+
     // check 2: make sure gcc laid the routines out in order
     //          (the first should have a smaller address than second, etc)
+    assert((char *)cpyjmp < (char *)cpyjmp2);
+
     // check 3: make sure there is "enough" code in the routines (not just
     //          a few instructions.
+    assert(cpyjmp_size > 2);
+
     // check 4: the final important check: make sure both routines are 
     //          identical (memcpy==0).
-    unimplemented();
+    for (unsigned i = 0; i < cpyjmp2_size; i++){
+        assert(((int *)cpyjmp)[i] == ((int *)cpyjmp2)[i]);
+    }
 }
 
 // copy <cpyjmp> to location <addr> and return a function pointer to the 
@@ -55,7 +82,10 @@ cpyjmp_t cpyjmp_relocate(uint32_t addr) {
     assert(addr >= cpyjmp_lowest_reloc_addr);
     cpyjmp_check();
 
-    unimplemented();
+    char *target = (char *)addr;
+    for (char *curr = (char *)cpyjmp; curr < (char *)cpyjmp2; curr++, target++) {
+        *target = *curr;
+    }
 
     return (cpyjmp_t)addr;
 }
